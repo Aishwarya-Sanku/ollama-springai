fcb5e86d5c1baca689c523caccb700629a7af5d8
Aishwarya <aishwaryasanku9@gmail.com>
2024-08-05 11:56:46 +0530

    updated  connected with react

--- ollama-springai/src/main/java/com/ollama/ai/AIController.java
+++ ollama-springai/src/main/java/com/ollama/ai/AIController.java
@@ -1,13 +1,15 @@
 package com.ollama.ai;
 
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
-
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/ai")
 public class AIController {
@@ -15,15 +17,25 @@ public class AIController {
 	
 	
 	private OllamaChatModel client;
-	private static final String PROMPT="WHAT IS java";
+	//private static final String PROMPT="WHAT IS java";
 	public AIController(OllamaChatModel client) {
 		this.client=client;
 	}
 	@GetMapping("/prompt")
-	public Flux<String> promptResponse(
+//	public Flux<String> promptResponse(
+//			@RequestParam("prompt") String prompt) {
+//	//String response=client.call(PROMPT);
+//		System.err.println("ccccccccccc");
+//		Flux<String> response=client.stream(prompt);
+//		return response;
+//	}
+	public ResponseEntity<String> promptResponse(
 			@RequestParam("prompt") String prompt) {
-	//String response=client.call(PROMPT);
-		Flux<String> response=client.stream(prompt);
-		return response;
+//	//String response=client.call(PROMPT);
+		System.err.println("ccccccccccc");
+		String response=client.call(prompt);
+		return ResponseEntity.ok(response);	
 	}
 }
+
+
